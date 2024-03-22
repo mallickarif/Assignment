1960da89150c5b4696989c25c7a700f2cb4c0dbe
mallickarif <mallickarif454@gmail.com>
2024-03-23 01:54:40 +0530

    add files

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+require("dotenv").config();
 const Product = require("./models/product.model.js");
 const Category = require("./models/category.model.js");
 const connectDB = require("./db/connection.js");
@@ -11,36 +11,40 @@ connectDB()
     console.log("MongoDB connection error", err);
 })
 
-const categories = [
-    { name: "Electronics", description: "Electronic devices and accessories" },
-    { name: "Clothing", description: "Clothing items for men and women" },
+Category.insertMany([
+    {  name: "Electronics", description: "Electronic devices and accessories" },
+    {  name: "Clothing", description: "Clothing items for men and women" },
     { name: "Books", description: "Books of various genres" }
-  ];
+  ]).then((res) => {
+    // console.log(res);
+  })
 
-  Category.insertMany(categories)
-  .then((categoryDocs) => {
-    console.log("Sample categories inserted:", categoryDocs);
+  // Category.insertMany(categories)
+  // .then((categoryDocs) => {
+    // console.log("Sample categories inserted:", categoryDocs);
 
-const products = [
-    { name: "Laptop", category_id: categoryDocs[0]._id, price: 1200 },
-    { name: "Smartphone", category_id: categoryDocs[0]._id, price: 800 },
-    { name: "T-shirt", category_id: categoryDocs[1]._id, price: 20 },
-    { name: "Jeans", category_id: categoryDocs[1]._id, price: 50 },
-    { name: "JavaScript: The Good Parts", category_id: categoryDocs[2]._id, price: 30 },
-    { name: "Clean Code", category_id: categoryDocs[2]._id, price: 35 }
-  ];
+ Product.insertMany([
+    { name: "Laptop", category_id: 100, price: 1200 },
+    { name: "Smartphone", category_id: 200, price: 800 },
+    { name: "T-shirt", category_id: 300, price: 20 },
+    { name: "Jeans", category_id: 400, price: 50 },
+    { name: "JavaScript: The Good Parts", category_id: 500, price: 30 },
+    { name: "Clean Code", category_id: 600, price: 35 }
+  ]).then((res) => {
+    // console.log(res);
+  })
 
-  return Product.insertMany(products);
-})
+  // return Product.insertMany(products);
+// })
 
-.then((productDocs) => {
-    console.log("Sample products inserted:", productDocs);
-})
-.catch((error) => {
-    console.error("Error generating sample data:", error);
-});
+// .then((productDocs) => {
+//     // console.log("Sample products inserted:", productDocs);
+// })
+// .catch((error) => {
+//     console.error("Error generating sample data:", error);
+// });
 
-Product.aggregate([
+   aggregate([
     {
       $lookup: {
         from: "categories",
@@ -50,12 +54,17 @@ Product.aggregate([
       }
     },
     {
-       $addFields: {
-           category_details: {
-            $arrayElemAt: ["$category_details", 0],
-           }
-       },
-    },
+      $addFields: {
+          category_details: {
+           $arrayElemAt: ["$category_details", 0],
+          }
+      },
+   },
+    // {
+      
+    //     $unwind: "$joinedData",
+      
+    // }
 ])
 .exec()
 .then((result) => {
